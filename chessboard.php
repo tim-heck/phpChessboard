87b2a0ccc531f445389f10ebc86393a3b8e0586f
tih99924 <tih99924@bethel.edu>
2016-02-04 14:19:33 -0600

    Change pink and yellow, final comments

--- chessboard.php
+++ chessboard.php
@@ -10,15 +10,18 @@ require_once '/Users/tih99924/Desktop/phpChessboard/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('/Users/tih99924/Desktop/phpChessboard/twig/');
 $twig = new Twig_Environment($loader);
 
+//Creates color array
 $color_array = array();
 
+//Fills color array with 'p' for pink or 'y' for yellow
 for ($row = 0; $row < 8; $row++) {
     for ($col = 0; $col < 8; $col++) {
+        //Every even numbered row & column are pink, odd are yellow
         if (($row + $col) % 2 == 0) {
-            $color_array[$row][$col] = 'w';
+            $color_array[$row][$col] = 'p';
         }
         else {
-            $color_array[$row][$col] = 'b';
+            $color_array[$row][$col] = 'y';
         }
     }
 }
